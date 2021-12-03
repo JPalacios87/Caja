f42b21657b73d37d172226ac43bd334696be637c
= <=>
2021-12-03 22:46:41 +0100

    Commit de prueba

--- Caja/Caja.java
+++ Caja/Caja.java
@@ -1,5 +1,15 @@
 package Caja;
 
 public class Caja {
+    int ancho;
+    int alto;
+    int profundo;
+
+public Caja() {
+        System.out.println("Ejecutando constructor");
+    }
+
+public int Caja() {
     
+}
 }
